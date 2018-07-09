3594148e93e3ebf12f8d7530c3459e3cab92fe3a
Marcio Galli <taboca@gmail.com>
2018-07-09 11:59:03 -0300

    Patch based in the https://github.com/react-native-community/react-native-camera/blob/master/docs/GradleUpgradeGuide.md

--- App.js
+++ App.js
@@ -1,49 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+'use strict';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+export default class App extends Component {
+  onSuccess(e) {
+    Linking
+      .openURL(e.data)
+      .catch(err => console.error('An error occured', err));
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
---
+++ README.md
@@ -0,0 +1,29 @@
+## Working example for QRCode app
+
+This project is the result of an attempt to get a QRCODE scanner Android application working using React-native and the following packages:
+
+* react-native-qrcode-scanner
+* react-native-camera
+
+Notice this was done in 9 July 2018. The purpose of this repository is to offer you an easy view about the changes that I had to do based on the initial state of a React-native application (the files produced by "react-native init MyApp"). Therefore this repository have only one major change:
+
+## The initial code
+
+The initial code refers to the files produced by the "react-native init YourAppName". You can see the initial code [here](https://github.com/taboca/doc-js-example-qrcode-cam-example/commit/c03e90476d7677dbf945c2687fe8b6e7e3f040e1).
+
+## Packages installation changes
+
+Once you have the initial React-native -based app, let's install the required packages dased in the documentation from [react-native-qrcode-scanner](https://github.com/moaazsidat/react-native-qrcode-scanner):
+
+```
+npm install react-native-camera --save
+react-native link react-native-camera
+npm install react-native-qrcode-scanner --save
+react-native link react-native-qrcode-scanner
+react-native link react-native-permissions
+react-native run-android
+```
+
+## Patching gradle
+
+[According to Upgrade gradle for Android Projects](https://github.com/react-native-community/react-native-camera/blob/master/docs/GradleUpgradeGuide.md) it' was necessary to patch the gradle infrastructure.
